a11a7b8a3965eef5c64612e1409f863a05c1f60f
Mohd Bilal <mohdbilal1102@gmail.com>
2026-04-18 00:55:23 +0530

    Middlewere updated

--- server.js
+++ server.js
@@ -3,36 +3,33 @@ const cors = require("cors");
 
 const app = express();
 
-//Middleware
-
-let corsOptions = {
-   origin : 'http://localhost:3000'
-}
+const corsOptions = {
+  origin: [
+    "http://localhost:3000",
+    "https://todo-app-omega-topaz.vercel.app"
+  ],
+  credentials: true
+};
 
 app.use(cors(corsOptions));
 
-
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
 
 const db = require("./models");
 
 db.mongoose
-    .connect(db.url, {
-        
-    })
-    .then(() => {
-        console.log("connected to the database");
-    })
-    .catch(err => {
-        console.log(" Cannot connect to database", err);
-        process.exit();
-    });
-
+  .connect(db.url)
+  .then(() => {
+    console.log("connected to the database");
+  })
+  .catch(err => {
+    console.log("Cannot connect to database", err);
+    process.exit();
+  });
 
 app.get("/", (req, res) => {
-    res.send("Todo backend is working");
+  res.send("Todo backend is working");
 });
 
 require("./routes/todo.routes")(app);
@@ -41,6 +38,5 @@ app.use("/api/auth", require("./routes/auth.routes"));
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
-
